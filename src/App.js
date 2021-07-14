e76afbf670c40332c811958ee18b3fcb62d512cf
solmin <solminkim03@naver.com>
2021-07-14 22:39:21 +0900

    21.07.14 _ api

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
 import React from "react";
 import {Component} from "react";
+import axios from "axios";
 
-function Food(props){           // {fav} -> props.fav 똑같음
-
-    return <h1> I like {props.fav} </h1>
-}
-
-function App() {
-  return (
-      <div>
-        <h1>Hello</h1>
-        <Food fav="kimchi"/>
-        <Food fav="ramen"/>
-        <Food fav="마라탕"/>
-          
-      </div>
-  );
-}
+// function Food(props){           // {fav} -> props.fav 똑같음
+//
+//     return <h1> I like {props.fav} </h1>
+// }
+//
+// function App() {
+//   return (
+//       <div>
+//         <h1>Hello</h1>
+//         <Food fav="kimchi"/>
+//         <Food fav="ramen"/>
+//         <Food fav="마라탕"/>
+//
+//       </div>
+//   );
+// }
 
 // state, setState() 메서드를 통해서 state 값 변경
-class App1 extends Component{
+class App extends Component{
     state={
-        count:0,
         isLoading:true,
         movies:[]
     };
-    add =()=>{
-        this.setState(cur=>({count:cur.count+1}))
+    getMovies = async ()=>{  // 함수가 비동기 , axios가 끝날 때까지 기다림
+        const movies = await axios.get("https://yts.mx/api/v2/list_movies.json")
     }
-    minus=()=>{
-        this.setState(cur=>({count:cur.count+1}));
-    }
-
     componentDidMount() {
-        setTimeout(()=>{
-            this.setState({isLoading:false});
-        },6000)
+        this.getMovies()
     }
 
     render() {
+        const{isLoading} = this.state;
         return(
-            <div>
-                {/*<h1>The number is : {this.state.count}</h1>
-                <button onClick={this.add}>ADD</button>
-                <button onClick={this.minus}>MINUS</button>*/}
 
+            <div>
                 {this.state.isLoading ? "Loading..":"We are ready"}
             </div>
         )
@@ -55,3 +47,6 @@ export default App;
 
 // render를 하면 호출되는 Life cycle -> componentDidMount
 // state를 선언하는건 필수가 아니다
+
+// axios
+// YTS api
